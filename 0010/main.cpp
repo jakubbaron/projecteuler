7e442e79cf27d969124e1f8e10ccc529f106a1c7
Jakub Baron <jakubaron@gmail.com>
2018-12-31 12:53:23 +0100

    Euler 0010 init

---
+++ 0010/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+//
+// Find the sum of all the primes below two million.
+
+int main(int argc, char** argv) {
+  return EXIT_SUCCESS;
+}
