f609114cc69416b7b582de56e8ed3926b9e6bd52
Jakub Baron <jakubaron@gmail.com>
2019-01-01 12:39:29 +0100

    Init 0015, Lattice paths

---
+++ 0015/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// Starting in the top left corner of a 2×2 grid, and only being able
+// to move to the right and down, there are exactly 6 routes to
+// the bottom right corner.
+// 
+// How many such routes are there through a 20×20 grid?
+
+int main(int argc, char** argv) {
+  constexpr auto grid_size = 2;
+
+  return EXIT_SUCCESS;
+}
