89a9c1bc490c4cb3215db82f7031ef82c038f208
Jakub Baron <jakubaron@gmail.com>
2019-01-15 00:14:01 +0000

    counting the score names

--- 0022/main.cpp
+++ 0022/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <algorithm>
 
 // Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
 // 
@@ -9,16 +10,28 @@
 // 
 // What is the total of all the name scores in the file?
 
+auto name_worth = [](const std::string& name) -> uint32_t {
+  uint32_t score = 0;
+  for(const auto& c: name) {
+    score += static_cast<uint32_t>(c - 'A' + 1);
+  } 
+  return score;
+};
+
 int main(int argc, char** argv) {
   std::ifstream infile("names.txt");
   std::string name;
   std::vector<std::string> names;
-  while (std::getline(infile, name)) {
+  while (std::getline(infile, name, ',')) {
+    name.erase(std::remove(name.begin(), name.end(), '\"' ), name.end());
     names.emplace_back(name);
   }
-  for(const auto& name: names) {
-    std::cout << name << std::endl; 
+  std::sort(names.begin(), names.end());
+  unsigned long long over_all_score = 0ULL;
+  for(size_t i = 0; i < names.size(); i++) {
+    over_all_score += static_cast<unsigned long long>(name_worth(names[i]) * (i + 1));
+    //std::cout << "[" << i << "] Current score: " << over_all_score << std::endl;
   }
-
+  std::cout << "Overall score: " << over_all_score << std::endl;
   return EXIT_SUCCESS;
 }
