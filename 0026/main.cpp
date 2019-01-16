bd28e1889f6814eeea4b62e18dbff8fc378cbeb4
Jakub Baron <jakubaron@gmail.com>
2019-01-16 22:58:59 +0000

    Fixing Euler0026, wrong starting compared lenghts for numbers > 100. Alg was finding just two reoccuring zeroes and was thinking that it's alright

--- 0026/main.cpp
+++ 0026/main.cpp
@@ -21,11 +21,10 @@
 
 auto get_as_fraction = [](int n, const int max_digits = 10000) {
   if(n == 0) { 
-    return std::make_pair(std::string("0"), int(0));
+    return std::make_pair(std::string("0"), size_t(0));
   }
 
   std::string output;
-
   int carry_over = 10;;
   int current_digits = 0;
   while(carry_over != 0 && current_digits < max_digits) {
@@ -33,18 +32,17 @@ auto get_as_fraction = [](int n, const int max_digits = 10000) {
     carry_over -= (next_place * n);
     std::string str_carry_over = std::to_string(carry_over);
     carry_over *= 10;
-    std::cout << "CarryOver: " << carry_over << " NextPlace: " << next_place << std::endl;
+    // std::cout << "CarryOver: " << carry_over << " NextPlace: " << next_place << std::endl;
     output += std::to_string(next_place);
     current_digits++;
   }
   bool repeated = false;
-  int len = 1;
-  std::string checking;
-  std::string second;
+  size_t start_len = std::to_string(n).size();
+  size_t len = start_len;
   for(int start = 0; start < output.length() / 2 && !repeated; start++) {
-    for(len = 1; len < output.length() / 2; len++) {
-      checking = output.substr(start, len);
-      second = output.substr(start + len, len );
+    for(len = start_len; len < output.length() / 2; len++) {
+      std::string checking = output.substr(start, len);
+      std::string second = output.substr(start + len, len );
       //std::cout << "Checking: " << checking << " second: " << second << std::endl;
       if(checking == second) {
         repeated = true;
@@ -52,20 +50,16 @@ auto get_as_fraction = [](int n, const int max_digits = 10000) {
       }
     }
   }
-  return std::make_pair(output, repeated ? len : 0);
+  return std::make_pair(output, repeated ? len : size_t(0));
 };
 
 int main(int argc, char** argv) {
   static auto constexpr upper_bound = 1000;
   std::vector<int> max_digits;
-  for(int i = 1; i < upper_bound; i++) {
+  for(int i = 0; i < upper_bound; i++) {
     const auto fr = get_as_fraction(i);
-    //std::cout << i << " " << fr.first << " " << fr.second << std::endl;
     max_digits.emplace_back(fr.second);
   }
-  for(int i = 0; i < max_digits.size(); i++) {
-    std::cout << (i+1) << " " << max_digits[i] << std::endl;
-  }
-  std::cout << "Max recurring cycle len: " << *(std::max_element(max_digits.begin(), max_digits.end())) << std::endl;
+  std::cout << "Number with max recurring cycle: " << std::distance(max_digits.begin(), std::max_element(max_digits.begin(), max_digits.end())) << std::endl;
   return EXIT_SUCCESS;
 }
