0b619d972cc2b9130cf9550c003bc5187ba1368f
Jakub Baron <jakubaron@gmail.com>
2019-01-16 00:16:15 +0000

    Init 26-50 problems with descriptions

---
+++ 0048/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
+//
+// Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
+
+int main(int argc, char** argv) {
+
+  return EXIT_SUCCESS;
+}
