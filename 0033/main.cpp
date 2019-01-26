66b049e3ca760e40f9143d930182645087080d83
Jakub Baron <jakubaron@gmail.com>
2019-01-26 17:40:11 +0000

    Euler 0033

--- 0033/main.cpp
+++ 0033/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <numeric>
 
 // The fraction 49/98 is a curious fraction, as an inexperienced mathematician in attempting to simplify it may incorrectly believe that 49/98 = 4/8, which is correct, is obtained by cancelling the 9s.
 // 
@@ -8,7 +9,28 @@
 // 
 // If the product of these four fractions is given in its lowest common terms, find the value of the denominator.
 
-int main(int argc, char** argv) {
+// https://en.cppreference.com/w/cpp/numeric/gcd
+// https://www.mathblog.dk/project-euler-33-fractions-unorthodox-cancelling-method/
 
+auto denominator_of_non_trivial_examples() noexcept -> int {
+  int denproduct = 1;
+  int nomproduct = 1;
+  
+  for (int i = 1; i < 10; i++) {
+    for (int den = 1; den < i; den++) {
+      for (int nom = 1; nom < den; nom++) {
+        if ((nom * 10 + i) * den == nom * (i * 10 + den)) {
+          denproduct *= den;
+          nomproduct *= nom;
+        }
+      }
+    }
+  }
+  denproduct /= std::gcd(nomproduct, denproduct);
+  return denproduct;
+}
+
+int main(int argc, char** argv) {
+  std::cout << "Denominator: " << denominator_of_non_trivial_examples() << std::endl;
   return EXIT_SUCCESS;
 }
