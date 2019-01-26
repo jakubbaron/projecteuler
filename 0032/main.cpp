caa4d35e8fe08126e4f9be1a133160d6277a0ed0
Jakub Baron <jakubaron@gmail.com>
2019-01-26 16:18:47 +0000

    String to int

--- 0032/main.cpp
+++ 0032/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <numeric>
+#include <set>
+#include <vector>
+#include <cmath>
 
 // We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
 // 
@@ -8,7 +12,31 @@
 // 
 // HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
 
-int main(int argc, char** argv) {
+auto string_to_int(const std::string& str) noexcept -> uint32_t {
+  // normally we'd check if there are only 0-9 characters, but we assume that
+  // we don't pass anything 'evil'
+  uint32_t out = 0;
+  static const std::vector<uint32_t> pows = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000};
+  for(int i = 0; i < str.length(); i++) {
+    out += static_cast<uint32_t>(str[i] - '0') * pows[str.length() - i - 1];
+  }
 
+  return out;
+}
+
+auto sum_of_pandigitals() noexcept -> uint32_t {
+  std::set<uint32_t> pandigitals;
+  std::string digits = "123456789";
+  // if we take longer than 4-digit number and multiply it by another number, we can get either 4-digit number or 5-digit number
+  // that means that we'll no longer be able to fit that our 1-9 pandigital
+  constexpr auto max_digits = 4;
+  do {
+    int first_number = 0;
+  } while(std::next_permutation(digits.begin(), digits.end()));
+  return std::accumulate(begin(pandigitals), end(pandigitals), 0);
+}
+
+int main(int argc, char** argv) {
+  // std::cout << string_to_int("123455") << std::endl;
   return EXIT_SUCCESS;
 }
