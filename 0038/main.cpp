307912590595303eb32b76f01a111ffb20e4b480
Jakub Baron <jakubaron@gmail.com>
2019-01-26 20:45:53 +0000

    Palindrome of products

--- 0038/main.cpp
+++ 0038/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
 
 // Take the number 192 and multiply it by each of 1, 2, and 3:
 // 
@@ -11,7 +14,34 @@
 // 
 // What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n > 1?
 
-int main(int argc, char** argv) {
+auto is_pandigital(const std::string& numbers) noexcept -> bool {
+  if(numbers.size() != 9) return false;
 
+  std::set<char> digits; 
+  for(const auto& c: numbers) {
+    if(digits.count(c)) {
+      return false;
+    }  
+    digits.insert(c);
+  }
+  return !digits.count('0') && (digits.size() == 9);
+}
+
+int main(int argc, char** argv) {
+  std::string max_padigital = "123456789";
+  for(int i = 1; i < 123456789; i++) {
+    std::string str;
+    int n = 1;
+    while(str.size() < 9) {
+      str += std::to_string(i * n++);
+    }
+    if(str.size() != 9) {
+      continue;
+    }
+    if(is_pandigital(str)) {
+      max_padigital = std::max(str, max_padigital); 
+    }
+  }
+  std::cout << max_padigital << std::endl;
   return EXIT_SUCCESS;
 }
