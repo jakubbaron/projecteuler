64e6733f88ffb01c3d3ed666b3372cd285c063d4
Jakub Baron <jakubaron@gmail.com>
2019-01-26 17:51:56 +0000

    Euler 0034, sum of numbers which factorials of particular digits is equal to the numbers itself

--- 0034/main.cpp
+++ 0034/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 // 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
 //
@@ -6,7 +7,38 @@
 // 
 // Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 
-int main(int argc, char** argv) {
+auto factorial(int n) noexcept -> int {
+  if(n > 10) return -1;
+  static std::vector<int> factorials(10, 0);
 
+  if(n == 0 || n == 1) {
+    return 1;
+  }
+
+  if(factorials[n] != 0) {
+    return factorials[n];
+  } else {
+    factorials[n] = n * factorial(n-1);
+    return factorials[n];
+  }
+}
+
+auto sum_of_numbers_equal_to_sum_of_factorials() noexcept -> int {
+  int sum = 0;
+  for(int i = 3; i < factorial(9); i++) {
+    int factorial_sum = 0;
+    for(const auto& c: std::to_string(i)) {
+      factorial_sum += factorial(static_cast<int>(c - '0'));
+    }
+    if(factorial_sum == i) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+int main(int argc, char** argv) {
+  auto sum = sum_of_numbers_equal_to_sum_of_factorials();
+  std::cout << "Sum is: " << sum << std::endl;
   return EXIT_SUCCESS;
 }
