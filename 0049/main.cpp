53e2daf97a81731b8f434d049e423009b29f9bab
Jakub Baron <jakubaron@gmail.com>
2019-01-29 21:45:05 +0000

    Counting distances, but that's not what we need

--- 0049/main.cpp
+++ 0049/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cmath>
+#include <set>
+#include <map>
 
 // The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by 3330, is unusual in two ways: (i) each of the three terms are prime, and, (ii) each of the 4-digit numbers are permutations of one another.
 // 
@@ -15,10 +18,9 @@ auto is_prime(int n) noexcept -> bool {
   return true;
 }
 
-auto primes_below(int n) noexcept -> const std::set<int> {
+auto primes_with_number_of_digits(size_t expected_digits) noexcept -> const std::set<int> {
     std::set<int> primes;
-    primes.emplace(2);
-    for(int candidate = 2; candidate <= n; candidate++) {
+    for(int candidate = std::pow(10, expected_digits - 1); candidate < std::pow(10, expected_digits); candidate++) {
       if(is_prime(candidate)) {
         primes.emplace(candidate);
       }
@@ -28,6 +30,20 @@ auto primes_below(int n) noexcept -> const std::set<int> {
 }
 
 int main(int argc, char** argv) {
-
+  const auto primes = primes_with_number_of_digits(4);
+  std::map<int, std::map<int, int> > distances;
+  for(const auto& prime: primes) {
+    for(const auto& prime2: primes) {
+      distances[prime][std::abs(prime-prime2)]++;
+    }
+  }
+  for(const auto& item: distances) {
+    for(const auto& distance: item.second) {
+      const auto& distance_count = distance.second;
+      if(distance_count > 2) {
+        std::cout << "Prime: " << item.first << " distance: " << distance.first << " count: " << distance_count << std::endl;
+      }
+    }
+  }
   return EXIT_SUCCESS;
 }
