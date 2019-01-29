be3e8c1fa78f62dc3b727f27aab3f5b5301d4623
Jakub Baron <jakubaron@gmail.com>
2019-01-29 23:31:06 +0000

    Euler 50, sum of consecutive primes

--- 0050/main.cpp
+++ 0050/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <cmath>
+#include <algorithm>
 
 // The prime 41, can be written as the sum of six consecutive primes:
 //
@@ -9,7 +13,54 @@
 // 
 // Which prime, below one-million, can be written as the sum of the most consecutive primes?
 
-int main(int argc, char** argv) {
+auto is_prime(int n) noexcept -> bool {
+  for(int i = 2; i < sqrt(n) + 1; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
 
+auto primes_below(int n) noexcept -> const std::vector<int> {
+    std::vector<int> primes;
+        primes.emplace_back(2);
+    for(int candidate = 2; candidate <= n; candidate++) {
+      if(is_prime(candidate)) {
+        primes.emplace_back(candidate);
+      }
+    }
+
+    return primes;
+}
+
+auto longest_consecutive_prime_below(int n) noexcept -> std::pair<int, int> {
+  const auto primes = primes_below(n);
+  std::vector<int> prime_sum(primes.size() + 1, 0);
+  for(int i = 0; i < primes.size(); i++) {
+    prime_sum[i+1] = prime_sum[i] + primes[i];
+  }
+  int number_of_primes = 0;
+  int result = 0;
+  for(int i = number_of_primes; i < prime_sum.size(); i++) {
+    for(int j = i - (number_of_primes + 1); j >= 0; j--) {
+      const auto sum = prime_sum[i] - prime_sum[j];
+      if(sum > n) {
+        break;
+      }
+
+      if(std::binary_search(begin(primes), end(primes), sum)) {
+        number_of_primes = i - j;
+        result = sum;
+      }
+    }
+  }
+
+  return std::make_pair(number_of_primes, result);
+}
+
+int main(int argc, char** argv) {
+  const auto result = longest_consecutive_prime_below(1000*1000);
+  std::cout << "Count: " << result.first << " Prime: " << result.second << std::endl;
   return EXIT_SUCCESS;
 }
